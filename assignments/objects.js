3f236104d437b179ce244fad9e0a3dab967e87fe
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-04-01 14:54:30 -0600

    answered 1, 2, 3 conversion

--- assignments/objects.js
+++ assignments/objects.js
@@ -30,7 +30,9 @@ const kennan = {
   "name": "Kennan",
   "email": "kdiben1@tinypic.com",
   "gender": "M",
-  "speak": "Hello, my name is Kennan!"
+  speak: function() {
+    console.log("Hello, my name is Kennan!")
+  }
 }
 
 const keven = {
@@ -52,7 +54,7 @@ const antonietta = {
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
   "gender": "F",
-  "multiplyNums": function(num1, num2) {
+  multiplyNums: function(num1, num2) {
     return num1 * num2;
   }
 }
@@ -75,10 +77,10 @@ console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
